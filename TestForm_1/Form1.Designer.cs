b81d2f4edc9c9dc36349c97cad9f239edb8c53d3
Naledi Kgabanyane <u25314247@up.ac.za>
2025-09-15 22:31:15 +0200

    TryPush

--- TestForm_1/Form1.Designer.cs
+++ TestForm_1/Form1.Designer.cs
@@ -149,7 +149,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(390, 390);
+            this.button1.Location = new System.Drawing.Point(406, 343);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(243, 106);
             this.button1.TabIndex = 5;
